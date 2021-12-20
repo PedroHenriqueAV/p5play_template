2e5c0897e426b7ed5b03611c1340736b0be92919
PedroHenriqueAV <95546453+PedroHenriqueAV@users.noreply.github.com>
2021-12-20 09:32:44 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,20 +7,25 @@ function setup() {
 
 function draw() 
 {
-  background(30);
+
 drawSprites();
 if (keyIsDown(DOWN_ARROW)){
-  box.position.y = box.position.y+5;
+  background ("blue");
+  box.shapeColor = "red";
 }
 if (keyIsDown(UP_ARROW)){
-  box.position.y = box.position.y-5;
+  background ("red");
+  box.shapeColor = "blue";
 }
 
 if (keyIsDown(LEFT_ARROW)){
-  box.position.x = box.position.x-5;
+  background ("green");
+  box.shapeColor = "yellow";
 }
 
 if (keyIsDown(RIGHT_ARROW)){
-  box.position.x = box.position.x+5;
+  background ("yellow");
+  box.shapeColor = "green";
 }
+
 }
